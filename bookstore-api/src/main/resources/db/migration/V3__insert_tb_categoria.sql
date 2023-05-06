63a1164a8242a6f8e58cfba42e77eb0298a288af
Matheus Rodrigues <matheus.arcelino@cidadania.gov.br>
2023-05-06 15:48:35 -0300

    Import do Projeto && Container e configurações de banco de dados

---
+++ bookstore-api/src/main/resources/db/migration/V3__insert_tb_categoria.sql
@@ -0,0 +1,8 @@
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 1', 'Descrição da categoria 1');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 2', 'Descrição da categoria 2');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 3', 'Descrição da categoria 3');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 4', 'Descrição da categoria 4');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 5', 'Descrição da categoria 5');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 6', 'Descrição da categoria 6');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 7', 'Descrição da categoria 7');
+insert into TB_CATEGORIA (NOME, DESCRICAO) values ('Categoria 8', 'Descrição da categoria 8');
